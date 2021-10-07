2e6f24689a1b38ef88cfe7c15c780d626fbb676d
Alex Salazar <alxanderapollo@gmail.com>
2021-10-07 13:17:11 -0400

    added spinner and reformated classes to functional components

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,47 +1,57 @@
 import React, { Component } from "react";
 import UserItem from "./UserItem";
-class Users extends React.Component {
-//   state = {
-//     // array of users that will come from the api
-//     users: [
-//       {
-//         id: "1",
-//         login: "mojombo",
-//         avatar_Url: "https://avatars0.githubusercontent.com/u/1?v=4",
-//         html_url: "https://github.com/mojombo/",
-//       },
-//       {
-//         id: "2",
-//         login: "defunkt",
-//         avatar_Url: "https://avatars0.githubusercontent.com/u/2?v=4",
-//         html_url: "https://github.com/defunkt/",
-//       },
-//       {
-//         id: "3",
-//         login: "pjhyett",
-//         avatar_Url: "https://avatars0.githubusercontent.com/u/3?v=4",
-//         html_url: "https://github.com/pjhyett/",
-//       },
-//     ],
-//   };
-  render() {
+import Spinner from "../layout/Spinner";
+import PropTypes  from 'prop-types';
+
+//destructure users and loading
+const Users = ({ users, loading }) => {
+  //   state = {
+  //     // array of users that will come from the api
+  //     users: [
+  //       {
+  //         id: "1",
+  //         login: "mojombo",
+  //         avatar_Url: "https://avatars0.githubusercontent.com/u/1?v=4",
+  //         html_url: "https://github.com/mojombo/",
+  //       },
+  //       {
+  //         id: "2",
+  //         login: "defunkt",
+  //         avatar_Url: "https://avatars0.githubusercontent.com/u/2?v=4",
+  //         html_url: "https://github.com/defunkt/",
+  //       },
+  //       {
+  //         id: "3",
+  //         login: "pjhyett",
+  //         avatar_Url: "https://avatars0.githubusercontent.com/u/3?v=4",
+  //         html_url: "https://github.com/pjhyett/",
+  //       },
+  //     ],
+  //   };
+
+  if (loading) return <Spinner/>;
+  else {
     return (
       <div style={userStyle}>
         {/* want to map through each user in the state array */}
         {/* must have a key in this case we can use ID */}
-        {this.props.users.map((user) => (
+        {users.map((user) => (
           // pass in the user id as a key, and the entire user.
           <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
   }
-}
+};
 
 const userStyle = {
   display: "grid",
   gridTemplateColumns: " repeat(3, 1fr)",
   gridGap: "1rem",
 };
+Users.propTypes ={
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
+}
 
 export default Users;
