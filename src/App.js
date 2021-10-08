07a437411d5970af15e27737b8676d90f1a4e888
Alex Salazar <alxanderapollo@gmail.com>
2021-10-07 22:44:28 -0400

    Added ids

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import axios from "axios";
 import React from "react";
 
 class App extends React.Component {
-
-    state = {
-      users: [], //stores all the users that come back from res.date --> axios
-      loading:false //loading will be to tell the user when the data is coming in, so will be using a spinner in place that says "loading..," until the data comes in
-    }
-
+  state = {
+    users: [], //stores all the users that come back from res.date --> axios
+    loading: false, //loading will be to tell the user when the data is coming in, so will be using a spinner in place that says "loading..," until the data comes in
+  };
 
   //life cycle method
   //good for loading data - runs when the component mounts
@@ -19,19 +17,20 @@ class App extends React.Component {
   //which is what we want
   //promise
   async componentDidMount() {
-    
     //the first thing will do is get a loading to appear while the data isn't fetched
     //since we are updating a state object
     //the way to do that is with this.setState
-    this.setState({loading:true})
+    this.setState({ loading: true });
 
-    const res = await axios.get("https://api.github.com/users")
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
     //want to put the users that come back from res.data into our state
     //within this app component
 
     //now that we have all our data and the data is no longer loading
     //using setState, reset the loading to false, and fill the users array with the data
-    this.setState({users:res.data, loading:false})
+    this.setState({ users: res.data, loading: false });
     // console.log(res.data)
   }
   render() {
