df1f53f1a63577c978429754983a46f1ce9df123
Alex Salazar <alxanderapollo@gmail.com>
2021-10-10 17:04:12 -0400

    made it so 10 repos appear at a time

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App extends React.Component {
   getUserRepos = async (username) => {
     this.setState({ loading: true });
     const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`    );
+      `https://api.github.com/users/${username}/repos?per_page=10&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`    );
     this.setState({ repos: res.data, loading: false });
   };
 
